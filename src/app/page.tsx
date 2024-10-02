29bf556eaafc992d7e0b7f8310f7763942626109
ankitjnvynr <ankitbkana@outlook.com>
2024-10-02 18:07:20 +0530

    what

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 import Image from "next/image";
-import HeroSection from '@/components/HeroSection'
+import { HeroSection, Services } from "@/components";
 
 export default function Home() {
   return (
-    <HeroSection />
+    <>
+      <HeroSection />
+      <Services />
+    </>
+
   );
 }
